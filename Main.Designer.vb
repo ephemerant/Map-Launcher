66a598a6e568a926785ca1a3fd16d6b4ca5b4ec3
ephemerant <clayton.mcg@gmail.com>
2015-04-29 03:24:09 -0400

    Republished
    Weeded out superfluous files

---
+++ Main.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFile
+        '
+        Me.txtFile.Location = New System.Drawing.Point(12, 42)
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.Size = New System.Drawing.Size(384, 20)
+        Me.txtFile.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(164, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Your Urban Terror .EXE:"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(321, 68)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(240, 68)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 3
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 103)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFile)
+        Me.Name = "Form1"
+        Me.Text = "Map Launcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtFile As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+
+End Class
---
+++ Main.vb
@@ -0,0 +1,138 @@
+﻿Imports System.IO
+Imports System.Security.Permissions
+
+Public Class Main
+
+    Function FileSplit(ByVal mapfile As String) As String()
+        Dim i = mapfile.LastIndexOf("\")
+        Return { _
+            mapfile.Substring(0, i), _
+            mapfile.Substring(i + 1, mapfile.Length - i - 1)}
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            My.Computer.Registry.ClassesRoot.CreateSubKey(".pk3").SetValue("", "pk3", Microsoft.Win32.RegistryValueKind.String)
+            My.Computer.Registry.ClassesRoot.CreateSubKey("pk3\shell\open\command").SetValue("", Application.ExecutablePath & _
+    " ""%l"" ", Microsoft.Win32.RegistryValueKind.String)
+            My.Computer.Registry.ClassesRoot.CreateSubKey(".bsp").SetValue("", "bsp", Microsoft.Win32.RegistryValueKind.String)
+            My.Computer.Registry.ClassesRoot.CreateSubKey("bsp\shell\open\command").SetValue("", Application.ExecutablePath & _
+    " ""%l"" ", Microsoft.Win32.RegistryValueKind.String)
+            My.Computer.Registry.ClassesRoot.CreateSubKey(".urtdemo").SetValue("", "urtdemo", Microsoft.Win32.RegistryValueKind.String)
+            My.Computer.Registry.ClassesRoot.CreateSubKey("urtdemo\shell\open\command").SetValue("", Application.ExecutablePath & _
+    " ""%l"" ", Microsoft.Win32.RegistryValueKind.String)
+
+            txtFile.Text = My.Settings.URT
+            Dim args() As String = System.Environment.GetCommandLineArgs(), demo = False
+
+            If args.Count >= 2 Then
+                ' Enable support for new q3ut4\download directory
+                Dim mapfile = args(1).Replace("\download", "")
+                Dim fsplit() As String = FileSplit(mapfile)
+                ' Go one directory up from the file
+                Dim _fsplit() As String = FileSplit(fsplit(0))
+
+                ' Check if map is in the correct location
+                If fsplit(1).Contains(".pk3") Then
+                    If _fsplit(1) <> "q3ut4" Then
+                        ' Try to copy it to user-defined URT folder
+                        If My.Settings.URT <> "" Then
+                            Dim _mapfile = FileSplit(My.Settings.URT)(0) & "\q3ut4\" & fsplit(1)
+
+                            ' Caution: will overwrite old file
+                            My.Computer.FileSystem.CopyFile(mapfile, _mapfile, True)
+
+                            ' Reconfigure variables
+                            mapfile = _mapfile
+                            fsplit = FileSplit(mapfile)
+                            _fsplit = FileSplit(fsplit(0))
+                        Else : Return
+                        End If
+                    End If
+                    ' Remove ".PK3"
+                    fsplit(1) = fsplit(1).Substring(0, fsplit(1).LastIndexOf("."))
+
+                ElseIf fsplit(1).Contains(".bsp") Then
+                    If _fsplit(1) <> "maps" Then
+                        MsgBox(".BSP must be in q3ut4\maps directory.")
+                        Me.Close()
+                        Return
+                    End If
+                    ' Remove ".BSP"
+                    fsplit(1) = fsplit(1).Substring(0, fsplit(1).LastIndexOf("."))
+                    ' Go one more directory up
+                    _fsplit = FileSplit(_fsplit(0))
+
+                ElseIf fsplit(1).Contains(".urtdemo") Then
+                    If _fsplit(1) <> "demos" Then
+                        ' Try to copy it to user-defined URT folder
+                        If My.Settings.URT <> "" Then
+                            Dim _mapfile = FileSplit(My.Settings.URT)(0) & "\q3ut4\demos\" & fsplit(1)
+
+                            ' Caution: will overwrite old file
+                            My.Computer.FileSystem.CopyFile(mapfile, _mapfile, True)
+
+                            ' Reconfigure variables
+                            mapfile = _mapfile
+                            fsplit = FileSplit(mapfile)
+                            _fsplit = FileSplit(fsplit(0))
+                        Else : Return
+                        End If
+                    End If
+                    ' Remove ".urtdemo"
+                    fsplit(1) = fsplit(1).Substring(0, fsplit(1).LastIndexOf("."))
+                    ' Go one more directory up
+                    _fsplit = FileSplit(_fsplit(0))
+                    demo = True
+
+                Else
+                    MsgBox("Invalid file type.")
+                    Me.Close()
+                    Return
+                End If
+
+                Dim cmd = ""
+
+                If My.Computer.FileSystem.FileExists(_fsplit(0) & "\Handler.exe") Then
+                    cmd = _fsplit(0) & "\Handler.exe"
+                ElseIf My.Computer.FileSystem.FileExists(_fsplit(0) & "\Quake3-UrT.exe") Then
+                    cmd = _fsplit(0) & "\Quake3-UrT.exe"
+                ElseIf My.Computer.FileSystem.FileExists(_fsplit(0) & "\ioUrbanTerror.exe") Then
+                    cmd = _fsplit(0) & "\ioUrbanTerror.exe"
+                Else
+                    MsgBox("Unable to locate Quake3-UrT.exe or ioUrbanTerror.exe in" & vbNewLine & _fsplit(0))
+                    Me.Close()
+                    Return
+                End If
+
+                Dim cmdargs = "+g_gametype 9 +sv_pure 0 " & " +devmap " & fsplit(1) & " +set fs_homepath """ & _fsplit(0) & """ +set fs_basepath """ & _fsplit(0) & """"
+
+                If demo Then cmdargs = cmdargs.Replace("+devmap", "+demo")
+
+                Process.Start(cmd, cmdargs)
+                Me.Close()
+            End If
+
+        Catch ex As Exception
+            MsgBox("An error occurred: " & ex.ToString)
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        My.Settings.URT = txtFile.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "C:\"
+        openFileDialog1.Filter = "Urban Terror .EXE |*.exe"
+        openFileDialog1.Title = "Please select your Urban Terror .EXE"
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            txtFile.Text = openFileDialog1.FileName
+        End If
+    End Sub
+End Class
